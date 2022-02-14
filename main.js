c81c47f9386eea868d5cb71ba49f4023d657315f
Saves <saves1484@gmail.com>
2022-02-13 16:09:58 -0800

    add duplicateCount function

--- main.js
+++ main.js
@@ -3,3 +3,22 @@
  function solution(str, ending) {
      return str.endsWith(ending);
  }
+
+ /*Write a function that will return the count of distinct case-insensitive alphabetic 
+ characters and numeric digits that occur more than once in the input string. 
+ The input string can be assumed to contain only alphabets (both uppercase and lowercase) 
+ and numeric digits.*/
+ function duplicateCount(text) {
+     const word = text.toLowerCase();
+     let count = 0;
+     const arr = [];
+     for (i = 0; i < word.length; i++) {
+         if (word.includes(word[i], i + 1)) {
+             if (arr.includes(word[i]) === false) {
+                 arr.push(word[i]);
+                 count++;
+             }
+         }
+     }
+     return count;
+ }
