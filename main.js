92eee472f33df148be8182011f5732eaed8120b7
Saves <saves1484@gmail.com>
2022-02-22 13:52:54 -0800

    add busStops function

--- main.js
+++ main.js
@@ -66,3 +66,24 @@ function justCoolStuff(arr1, arr2) {
         }
       }
     }
+
+    /* You are provided with a list (or array) of integer pairs. Elements of each pair represent number
+     of people get into bus (The first item) and number of people get off the bus (The second item) in a bus stop.
+     Your task is to return number of people who are still in the bus after the last bus station (after the last
+       array). */
+    function busStops(arr){
+      let people = 0;
+      for (i = 0; i < arr.length; i++){
+        for (j = 0; j < arr[i].length; j++){
+          if (j === 0) {
+            people += arr[i][j];
+            
+          } else if (j === 1) {
+            people -= arr[i][j];
+            
+          }
+        }
+        j = 0
+      }
+      return people;
+    }
